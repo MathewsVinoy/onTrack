173918caf95dc0805d2c025db324ce5c4e924f9d
mathews <mathewsvinoy@gmail.com>
2024-12-31 15:27:01 +0530

    trying impove the look of of add page

--- lib/pages/addpage.dart
+++ lib/pages/addpage.dart
@@ -11,42 +11,47 @@ class AddPage extends StatelessWidget {
   Widget build(BuildContext context) {
     TextEditingController titleController = TextEditingController();
     TextEditingController dripController = TextEditingController();
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextField(
-          decoration: const InputDecoration(
-            label: Text("Title"),
-          ),
-          controller: titleController,
-        ),
-        TextField(
-          decoration: const InputDecoration(
-            label: Text("note about the content"),
-          ),
-          controller: dripController,
-        ),
-        ElevatedButton(
-          onPressed: () async {
-            Random random = Random();
-            int idno = random.nextInt(100000);
-            Map<String, dynamic> data = {
-              "id": idno,
-              "task": titleController.text,
-              "discription": dripController.text
-            };
-            await DataBaseHelper().insertDb(data, 'tasks');
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (ctx) => const NaveBarComp(),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                label: Text("Title"),
               ),
-            );
-          },
-          child: const Text("Submit"),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                label: Text("note about the content"),
+              ),
+              controller: dripController,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                Random random = Random();
+                int idno = random.nextInt(100000);
+                Map<String, dynamic> data = {
+                  "id": idno,
+                  "task": titleController.text,
+                  "discription": dripController.text
+                };
+                await DataBaseHelper().insertDb(data, 'tasks');
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (ctx) => const NaveBarComp(),
+                  ),
+                );
+              },
+              child: const Text("Submit"),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
